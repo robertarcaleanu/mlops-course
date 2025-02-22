99ce3a61b6e77f509db33e0a0f59c2ad517e39e0
Robert Arcaleanu <alumnerobert@gmail.com>
2025-02-22 15:51:02 +0000

    start tests

---
+++ tests/test_transformer.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+from online_inference_pipeline.transform import Transformer
+
+
+def test_map_binary_column_to_int():
+    transformer = Transformer()
+    df = pd.DataFrame({
+        "housing": ["yes", "no"],
+        "loan": ["no", "yes"],
+        "default": ["no", "yes"],
+    })
+    transformed_df = transformer._map_binary_column_to_int(df)
+    
+    assert transformed_df["housing"].tolist() == [1, 0]
+    assert transformed_df["loan"].tolist() == [0, 1]
+    assert transformed_df["default"].tolist() == [0, 1]
+
+
+def test_map_month_to_int():
+    transformer = Transformer()
+    df = pd.DataFrame({"month": ["jan", "feb", "mar"]})
+    transformed_df = transformer._map_month_to_int(df)
+    
+    assert transformed_df["month"].tolist() == [1, 2, 3]
